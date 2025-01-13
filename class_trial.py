a85e33812a3687c1e8a5003ff49f4091bfe3e2bf
Harikrishnan S <127649532+Hari070701@users.noreply.github.com>
2025-01-13 18:44:02 +0530

    Adding class_trial.py to repo

---
+++ class_trial.py
@@ -0,0 +1,10 @@
+class Student:
+
+    def __init__(self, name, roll_no):
+        self.name = name
+        self.roll_no = roll_no
+
+sl = Student ("John Doe", 17)
+print (type (Student))
+print(sl.name)
+print (sl.roll_no)
